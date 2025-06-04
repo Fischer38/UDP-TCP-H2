05252d7c0eaa3ae1ec0ab9fdd548575b06951925
Nikolaj F. larsen <nikolajfischer38@gmail.com>
2025-06-04 09:21:40 +0200

    Add user input for customizable messages in UDPClient

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -1,6 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.Scanner;
 
 public class UDPClient {
     public static void main(String[] args) {
@@ -8,8 +12,11 @@ public class UDPClient {
             InetAddress serverAddress = InetAddress.getByName("10.108.149.31");
             int serverPort = 9876;
             DatagramSocket clientSocket = new DatagramSocket(); // Ingen binding til serverens adresse!
+            Scanner scanner = new Scanner(System.in);
 
-            byte[] sendData = "Hej server!".getBytes();
+            System.out.print("Skriv besked: ");
+            String besked = scanner.nextLine();
+            byte[] sendData = besked.getBytes();
             byte[] receiveData = new byte[1024];
 
             // Send besked til serveren
