441e5560d1b8d5ca7496423e12bb493d8a8b2326
Nikolaj F. larsen <nikolajfischer38@gmail.com>
2025-06-04 09:21:58 +0200

    Add user input for customizable messages in UDPClient

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -1,6 +1,3 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
