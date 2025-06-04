c31de172ef956b9483cf5931b37aa9cc2423d0fd
Nikolaj F. larsen <nikolajfischer38@gmail.com>
2025-06-04 09:43:00 +0200

    Add user input for customizable messages in UDPClient

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -11,22 +11,26 @@ public class UDPClient {
             DatagramSocket clientSocket = new DatagramSocket(); // Ingen binding til serverens adresse!
             Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Skriv besked: ");
-            byte[] besked = scanner.nextLine().getBytes();
-            byte[] receiveData = new byte[1024];
+            do{
+                System.out.print("Skriv besked: ");
+                byte[] besked = scanner.nextLine().getBytes();
+                byte[] receiveData = new byte[1024];
 
-            // Send besked til serveren
-            DatagramPacket sendPacket = new DatagramPacket(besked, besked.length, serverAddress, serverPort);
-            clientSocket.send(sendPacket);
+                if (besked.length == 0 || besked[0] == '\n' || besked[0] == '\r') {
+                    break;
+                }
 
-            // Modtag serverens svar
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            clientSocket.receive(receivePacket);
+                DatagramPacket sendPacket = new DatagramPacket(besked, besked.length, serverAddress, serverPort);
+                clientSocket.send(sendPacket);
 
-            String svar = new String(receivePacket.getData(), 0, receivePacket.getLength());
-            System.out.println("Svar fra server: " + svar);
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                clientSocket.receive(receivePacket);
 
+                String svar = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                System.out.println("Svar fra server: " + svar);
+            }while (true);
             clientSocket.close();
+            scanner.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
