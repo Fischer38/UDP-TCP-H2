6b4bc8285348b6ae3a37b2d519c76a06cab72282
Nikolaj F. larsen <nikolajfischer38@gmail.com>
2025-06-04 09:11:28 +0200

    Simplify UDPClient by removing user input loop and refactoring message handling logic.

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -1,35 +1,29 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.Scanner;
 
 public class UDPClient {
     public static void main(String[] args) {
         try {
-            Scanner scanner = new Scanner(System.in);
-            DatagramSocket clientSocket = new DatagramSocket();
-            InetAddress IPAddress = InetAddress.getByName("localhost");
-            byte[] sendData;
+            InetAddress serverAddress = InetAddress.getByName("10.108.149.31");
+            int serverPort = 9876;
+            DatagramSocket clientSocket = new DatagramSocket(); // Ingen binding til serverens adresse!
+
+            byte[] sendData = "Hej server!".getBytes();
             byte[] receiveData = new byte[1024];
-            
-            while (true) {
-                System.out.print("Indtast besked: ");
-                String besked = scanner.nextLine();
-                
-                sendData = besked.getBytes();
-                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-                clientSocket.send(sendPacket);
-                
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                clientSocket.receive(receivePacket);
-                
-                String modtagetBesked = new String(receivePacket.getData(), 0, receivePacket.getLength());
-                System.out.println("Fra server: " + modtagetBesked);
-                
-                if (besked.equalsIgnoreCase("exit")) break;
-            }
+
+            // Send besked til serveren
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, serverPort);
+            clientSocket.send(sendPacket);
+
+            // Modtag serverens svar
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            clientSocket.receive(receivePacket);
+
+            String svar = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            System.out.println("Svar fra server: " + svar);
+
             clientSocket.close();
-            scanner.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
